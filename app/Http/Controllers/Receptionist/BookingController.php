a5159c477224153d46a8928bb689f87d50426e19
Pradipta Bhuin <pradiptabhuin@Pradiptas-MacBook-Pro.local>
2025-05-25 11:47:32 +0530

    67

--- app/Http/Controllers/Receptionist/BookingController.php
+++ app/Http/Controllers/Receptionist/BookingController.php
@@ -28,7 +28,7 @@ class BookingController extends Controller
     public function mobile(Request $request)
     {
         // $UserData = User::where('mobile', $request->mobile)->first();
-        $UserData = User::where(DB::raw('RIGHT(mobile, 10)'), $request->mobile)->first();
+        $UserData = User::where('cdc', $request->mobile)->first();
         
         echo json_encode($UserData);
     }
